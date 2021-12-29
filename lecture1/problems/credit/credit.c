1765345c02e4eeaf2a2b2ffea5d95da007625a83
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2021-12-29 18:14:18 -0300

    Validate credit card using Luhn’s Algorithm

--- lecture1/problems/credit/credit.c
+++ lecture1/problems/credit/credit.c
@@ -1,15 +1,44 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int count_digit(long long number)
 {
-    long int card = 4003600000000014;
     int count = 0;
-
-    while (card != 0) {
-        card = card/10;
+    while (number != 0) {
+        number = number/10;
         count++;
     }
 
-    printf("%d", count);
+    return count;
+}
+
+int main()
+{
+    long long card = 4003600000000014;
+    int numbers = count_digit(card);
+    int card_array[numbers];
+    int product = 0;
+
+    int i = 0;
+    while (card != 0) {
+        card_array[i] = card % 10;
+        card = card / 10;
+        i++;
+    }
+
+    for (int i = numbers - 1; i >= 0; i--) {
+        if (i % 2 == 1) {
+            if (card_array[i] * 2 >= 10) {
+                product += (2 * card_array[i]) % 10 + (2 * card_array[i]) / 10;
+            }
+            else {
+                product += 2 * card_array[i];
+            }
+            printf("Product: %d\n", product);
+        } else {
+            product += card_array[i];
+        }
+    }
+    return 0;
 }
