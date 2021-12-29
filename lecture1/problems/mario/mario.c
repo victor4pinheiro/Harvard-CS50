9c73c2a5e17d99b755c30315f43e2b04b79453b4
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2021-12-29 13:13:32 -0300

    Change to dynamic pyramids Add variable to get max numbers to print dynamically pyramids according to input

--- lecture1/problems/mario/mario.c
+++ lecture1/problems/mario/mario.c
@@ -3,15 +3,21 @@
 
 int main()
 {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 7; j >= 0; j--) {
+    int max_numbers = get_int("Height: ");
+
+    if (max_numbers < 1 || max_numbers > 8) {
+        max_numbers = get_int("Height: ");
+    }
+
+    for (int i = 0; i < max_numbers; i++) {
+        for (int j = max_numbers - 1; j >= 0; j--) {
             if (j <= i) {
                 printf("#");
             } else {
                 printf(" ");
             }
         }
-        printf(" ");
+        printf("  ");
 
         for (int j = 0; j <= i; j++) {
             printf("#");
