6f84f83ed7189940fd8d80574d322dc96ac105c8
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-01-12 23:15:53 -0300

    Add find_min to find the lowest vote count

--- lecture3/problems/runoff/runoff.c
+++ lecture3/problems/runoff/runoff.c
@@ -156,8 +156,16 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    int min = candidates[0].votes;
+    for (int i = 0; i < candidate_count; i++) {
+        if (!candidates[i].eliminated) {
+            if (min > candidates[i].votes) {
+                min = candidates[i].votes;
+            }
+        }
+    }
+
+    return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
