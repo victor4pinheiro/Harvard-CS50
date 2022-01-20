30bb56f29cad307d5d68b97816ce05f0d32cc464
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-01-20 17:53:20 -0300

    Finally! Add blur function

--- lecture4/problems/filter-more/helpers.c
+++ lecture4/problems/filter-more/helpers.c
@@ -28,14 +28,11 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         }
     }
 
-    // Reflect image horizontally
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             image[i][j] = new_image[i][j];
         }
     }
-
-    
        
     return;
 }
@@ -43,8 +40,41 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE new_image[height][width];
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++)
+            new_image[i][j] = image[i][j];
+    }
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int sum_red = 0;
+            int sum_green = 0;
+            int sum_blue = 0;
+            int count = 0;
+
+            for (int k = -1; k <= 1; k++) {
+                for (int l = -1; l <= 1; l++) {
+                    if (i + k < 0 || i + k >= height || j + l < 0 || j + l >= width)
+                        continue;
+
+                    sum_red += new_image[i + k][j + l].rgbtRed;
+                    sum_green += new_image[i + k][j + l].rgbtGreen;
+                    sum_blue += new_image[i + k][j + l].rgbtBlue;
+                    count++;
+                }
+            }
+
+            image[i][j].rgbtRed = round(sum_red / (float) count);
+            image[i][j].rgbtGreen = round(sum_green / (float) count);
+            image[i][j].rgbtBlue = round(sum_blue / (float) count);
+        }
+    }
+
     return;
 }
+
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
