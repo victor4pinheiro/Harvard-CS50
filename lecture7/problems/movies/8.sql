b4342b9915d6c1f31b770b36b1d5349f6a1cec35
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-05-30 15:21:57 -0300

    feat: list the names of all people who starred in Toy Story

---
+++ lecture7/problems/movies/8.sql
@@ -0,0 +1,8 @@
+SELECT name
+FROM people
+WHERE id IN (
+        SELECT person_id
+        FROM stars
+            JOIN movies ON stars.movie_id = movies.id
+        WHERE movies.title = "Toy Story"
+    );
